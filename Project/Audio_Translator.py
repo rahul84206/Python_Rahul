0e6e0b272ef7e7086edba55d2fd1d852a33fa1cf
Rahul Khandait <90828823+rahul84206@users.noreply.github.com>
2023-06-28 02:30:11 +0530

    code corrected

--- Project/Audio_Translator.py
+++ Project/Audio_Translator.py
@@ -3,20 +3,23 @@ import speech_recognition as sr
 import gtts
 import playsound
 
+
+input_lang = 'en'
+output_lang='hi'
+
 recognizer = sr.Recognizer()
+# try:
+with sr.Microphone() as source:
+    print('Speak Now')
+    voice=recognizer.listen(source)
+    text=recognizer.recognize_google(voice,language=input_lang)
+    print(text)
+# except:
+#     pass
+
 translator=gt.Translator()
-input_lang = 'hi-IN'
-output_lang='bn'
-try:
-    with sr.Microphone() as source:
-        print('Speak Now')
-        voice=recognizer.listen(source)
-        text=recognizer.recognize_google(voice,language=input_lang)
-        print(text)
-except:
-    pass
-translated=translator.translate(text,dest=output_lang)
-print(translated.text)
+translation=translator.translate(text,dest=output_lang)
+print(translation.text)
 converted_audio=gtts.gTTS(translated.text,lang=output_lang)
 converted_audio.save('translated_lang.mp3')
 playsound.playsound('translated_lang.mp3')
